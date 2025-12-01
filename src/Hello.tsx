9c4cfdeb7a4f1aee20edfb6c6188eff056815363
khanhhy <ledangkhanhhy@gmail.com>
2025-12-01 11:12:25 +0700

    first commit

---
+++ src/Hello.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Hello: React.FC = () => {
+  return (
+    <div className="text-center">
+      <h1 className="text-violet-300 text-6xl">Hello World!</h1>
+    </div>
+  );
+};
+
+export default Hello;
