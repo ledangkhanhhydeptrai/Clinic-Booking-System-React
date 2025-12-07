a5bb1d2f45b93d0499eba889d418d116eedcbf1b
khanhhy <ledangkhanhhy@gmail.com>
2025-12-07 13:29:30 +0700

    feat: enhance authentication flow with role management and navigation

--- src/features/auth/useAuth.ts
+++ src/features/auth/useAuth.ts
@@ -1,15 +1,32 @@
 import { useMutation } from "@tanstack/react-query";
 import { useDispatch } from "react-redux";
-import { setToken } from "./authSlice";
+import { setRole, setToken } from "./authSlice";
 import { loginApi } from "./api";
+import { useNavigate } from "react-router-dom";
 
 export function useLogin() {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   return useMutation({
     mutationFn: loginApi,
     retry: false,
     onSuccess: (data) => {
       dispatch(setToken(data.data.token));
+      dispatch(setRole(data.data.role));
+      switch (data.data.role) {
+        case "ADMIN":
+          navigate("/admin");
+          break;
+        case "DOCTOR":
+          navigate("/doctor");
+          break;
+        case "PATIENT":
+          navigate("/");
+          break;
+        default:
+          navigate("/");
+          break;
+      }
     }
   });
 }
--- src/routes/privateRoutes.tsx
+++ src/routes/privateRoutes.tsx
@@ -1,7 +1,9 @@
+import DoctorPage from "../pages/admin/doctor/DoctorPage";
 import Homepage from "../pages/home/Homepage";
 import ProfilePage from "../pages/profile/ProfilePage";
 
 export const privateRoutes = [
   { path: "/", element: <Homepage /> },
-  { path: "/profile", element: <ProfilePage /> }
+  { path: "/profile", element: <ProfilePage /> },
+  { path: "/admin", element: <DoctorPage /> }
 ];
